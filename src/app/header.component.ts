9c18ddb9312eda78217ec8d4070c9100a6e40185
shasank <shasankt01@gmail.com>
2024-08-26 00:21:51 +0530

    header added

---
+++ src/app/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  standalone: true,
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
